a8fd827dee3daa0d727f9fbb181e8a45b05a5e91
dymasalfin <71883719+dymasalfin@users.noreply.github.com>
2021-05-11 12:03:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+	header("location: welcome.php");
+}
+?>
+
+<html>
+<head>
+	<title>HALAMAN LOGIN</title>
+</head>
+<body>
+	<center><h1>LOGIN</h1></center>
+	
+	<div style="color: red;margin-bottom: 15px;">
+		<?php
+		if(isset($_COOKIE["message"])){
+			echo $_COOKIE["message"];
+		}
+		?>
+	</div>
+	
+	<center><form method="post" action="Login.php">
+		<label>Username</label><br>
+		<input type="text" name="username" placeholder="Username"><br><br>
+		
+		<label>Password</label><br>
+		<input type="password" name="password" placeholder="Password"><br><br>
+		
+		<button type="submit">Login</button>
+	</form></center>
+</body>
+</html>
