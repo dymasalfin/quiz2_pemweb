7b91247b2b2f0d53b9c2928024917cbbe4e9e583
dymasalfin <71883719+dymasalfin@users.noreply.github.com>
2021-05-15 21:28:04 +0700

    style form daftar

--- form-pendaftaran.php
+++ form-pendaftaran.php
@@ -1,29 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Form Pendaftaran</title>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Form Pendaftaran</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
-<body>
-<div class="container">
-<h2>Form Pendaftaran</h2>
+  </head>
+  <body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <center><div class="container">
+    <h2>Form Pendaftaran</h2>
     <form action="simpan-pendaftaran.php" method="post">
         <div class="form-group">
             <label>Username:</label>
-            <input type="text" name="username" class="form-control" placeholder="Masukan Username" />
+            <div class="col-md-4 col-sm-4 ">
+              <input type="text" name="username" class="form-control" placeholder="Masukan Username" />
+            </div>
         </div>
-		<div class="form-group">
+		    <div class="form-group">
             <label>Password:</label>
+            <div class="col-md-4 col-sm-4 ">
             <input type="password" name="password" class="form-control" placeholder="Masukan Password" />
+            </div>
         </div>
         <div class="form-group">
             <label>Nama:</label>
+            <div class="col-md-4 col-sm-4 ">
             <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" />
+            </div>
         </div>
 
         <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 
     </form>
-</div>
-</body>
+</div></center>
+  </body>
 </html>
